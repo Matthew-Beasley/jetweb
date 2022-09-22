8d13042125cd9ae29ccbb565d795559a6abfcb62
Matthew Beasley <matthewbeasley@Matthews-MacBook-Pro.local>
2022-09-22 14:41:31 -0700

    Added sub pages, finished all resolutions except phone on home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
+import {Link} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="leftpanel">
+        <div className="app-heading">
+          <p>
+            JETWEB <br/>
+            BRINGING YOUR WEB <br/> IDEAS TO LIFE
+          </p>
+        </div>
+        <div className="app-text">
+          Compelling responsive web sites that will delight your audience, efficient robust backend solutions
+        </div>
+      </div>
+      <div className="rightpanel">
+       <div className="links">
+        <Link to='/contact'>Contact</Link>
+        <Link to='/products'>Products</Link>
+        <Link to='/portfolio'>Portfolio</Link>
+        <Link to='/aboutme'>About Me</Link>
+       </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Contact from './Contact';
+import Products from './Products';
+import AboutMe from './AboutMe';
+import Portfolio from './Portfolio';
 import reportWebVitals from './reportWebVitals';
+import { RecoilRoot } from 'recoil';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route exact path='/' element={<App />}></Route>
+      <Route path='/contact' element={<Contact />}></Route>
+      <Route path='/products' element={<Products />}></Route>
+      <Route path='/aboutme' element={<AboutMe />}></Route>
+      <Route path='/portfolio' element={<Portfolio />}></Route>
+    </Routes>
+  </BrowserRouter>
 );
+
+/*
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
+root.render(
+  <CookiesProvider>
+  <BrowserRouter>
+    <Routes>
+      <Route exact path='/' element={< LandingPage />}></Route>
+    </Routes>
+  </BrowserRouter>
+  </CookiesProvider>
+);
+*/
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
